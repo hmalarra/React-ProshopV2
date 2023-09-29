db3bc67d139adb16e40ea6b98ebd5f7dc01e9d7e
PTHMAL1 <hugo.malarranha@atradius.com>
2023-09-29 10:59:31 +0100

    modified custom bootstrap lib

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,17 @@
 
 import { Navbar, Nav, Container} from 'react-bootstrap';
 import { FaShoppingCart, FaUser} from 'react-icons/fa';
+import logo from '../assets/logo.png'
 
 const Header = () => {
   return (
     <header>
         <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
             <Container>
-                <Navbar.Brand href='/'>ProShopV2</Navbar.Brand>
+                <Navbar.Brand href='/'>
+                    <img src={logo} alt='Proshop'/>
+                    ProShopV2
+                    </Navbar.Brand>
                 <Navbar.Toggle aria-controls='basic-navbar-nav'/>
                 <Navbar.Collapse id='basic-navbar-nav'>
                     <Nav className='ms-auto'>
